3a2034baf9f287a70f942ba31642762d3de7ddee
Tim Burkhart <burkhartt@digitalev.com>
2011-08-16 16:52:39 -0500

    Added string.IsNullOrEmpty

---
+++ L2R/L2R/StringHelpers.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    public static class StringHelpers
+    {
+        public static bool IsNullOrEmpty(this string @string)
+        {
+            return string.IsNullOrEmpty(@string);
+        }
+    }
+}
---
+++ L2RTests/L2RTests/StringHelpersTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using Should;
+
+namespace L2RTests
+{
+    [TestFixture]
+    public class StringHelpersTests
+    {
+        [Test]
+        public void When_I_have_an_empty_string_then_return_true_when_testing_if_it_is_null_or_empty()
+        {
+            "".IsNullOrEmpty().ShouldBeTrue();
+        }
+
+        [Test]
+        public void When_I_have_a_null_string_then_return_true_when_testing_if_it_is_null_or_empty()
+        {
+            string a = null;
+            a.IsNullOrEmpty().ShouldBeTrue();
+        }
+
+        [Test]
+        public void When_I_have_a_non_empty_and_non_null_string_then_return_false_when_testing_if_it_is_null_or_empty()
+        {
+            var a = "stuff";
+            a.IsNullOrEmpty().ShouldBeFalse();
+        }
+    }
+}
